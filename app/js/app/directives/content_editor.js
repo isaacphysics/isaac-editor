fb596287e527bbd82bea0d6a523d6fcbfecbcf36
Andrea Franceschini <andrea.franceschini@gmail.com>
2016-10-17 10:44:37 +0100

    No, *this* fixes checkboxes.

--- app/js/app/directives/content_editor.js
+++ app/js/app/directives/content_editor.js
@@ -1424,25 +1424,12 @@ define(["react", "jquery", "codemirrorJS", "showdown/showdown", "showdown/extens
 		},
 
 		onCheckboxChange: function(key, e) {
-			// if (key != "requireUnits") return;
-
 			console.log("New checkbox state:", e.target.checked, "for key:", key);
 
 			// newVal must be a doc
 			var oldDoc = this.props.doc;
 			var newDoc = $.extend({}, oldDoc);
 			newDoc[key] = e.target.checked;
-
-			this.onDocChange(this, oldDoc, newDoc);
-		},
-
-		onRandomiseChoicesChange: function(e) {
-			console.log("Randomise choices: ", e.target.checked);
-
-			// newVal must be a doc
-			var oldDoc = this.props.doc;
-			var newDoc = $.extend({}, oldDoc);
-			newDoc.randomiseChoices = e.target.checked;
 
 			this.onDocChange(this, oldDoc, newDoc);
 		},
@@ -1586,7 +1573,7 @@ define(["react", "jquery", "codemirrorJS", "showdown/showdown", "showdown/extens
 								</div>
 								<div className="row" style={{display: this.props.doc.type == "isaacMultiChoiceQuestion" ? "block" : "none"}}>
 									<div ref="randomiseChoicesCheckbox" className="small-6 small-offset-6 columns">
-										<label><input type="checkbox" checked={this.props.doc.randomiseChoices} onChange={this.onRandomiseChoicesChange.bind(this)} />Randomise Choices</label>
+										<label><input type="checkbox" checked={this.props.doc.randomiseChoices} onChange={this.onCheckboxChange.bind(this, "randomiseChoices")} />Randomise Choices</label>
 									</div>
 								</div>
 							</div>
