d4144e143a7e039489e74d5d049bc657800adc4e
Andrea Franceschini <andrea.franceschini@gmail.com>
2019-11-07 15:08:17 +0000

    Add exam board select box to glossary terms.

--- app/js/app/directives/react_classes/GlossaryTermBlock.js
+++ app/js/app/directives/react_classes/GlossaryTermBlock.js
@@ -40,6 +40,13 @@ define(["react", "jquery"], function(React,$) {
                 this.onDocChange(this, oldDoc, newDoc);
             },
 
+            onExamBoardChange: function(c, oldVal, newVal) {
+                var oldDoc = this.props.doc;
+                var newDoc = $.extend({}, oldDoc);
+                newDoc.examBoard = c.target.value;
+                this.onDocChange(this, oldDoc, newDoc);
+            },
+
             toggle: function(attribute) {
                 return function() {
                     var oldDoc = this.props.doc;
@@ -53,17 +60,34 @@ define(["react", "jquery"], function(React,$) {
                 if (typeof this.props.doc.autoId == 'undefined') {
                     this.props.doc.autoId = true;
                 }
+                if (typeof this.props.doc.examBoard == 'undefined') {
+                    this.props.doc.examBoard = '';
+                }
                 return (
                     <Block type="glossaryTerm" blockTypeTitle="Glossary term" doc={this.props.doc} onChange={this.onDocChange}>
-                        <div className="small-3 columns">
+                        <div className="small-5 columns">
                             <div className="row">
                                 <input type="text" value={this.props.doc.value} onChange={this.onContentChange} placeholder="Glossary term" />
-                                <input type="checkbox" checked={this.props.doc.autoId} onChange={this.onAutoIdToggle} /><label>Automatic IDs</label>
-                                {this.props.doc.autoId && <p>ID: {this.props.doc.id}</p>}
-                                {!this.props.doc.autoId && <input type="text" value={this.props.doc.id} onChange={this.onIdChange} placeholder="ID" />}
+                            </div>
+                            <div className="row">
+                                <div className="small-7 columns">
+                                    <input type="checkbox" checked={this.props.doc.autoId} onChange={this.onAutoIdToggle} /><label>Automatic IDs</label>
+                                    {this.props.doc.autoId && <p>ID: {this.props.doc.id}</p>}
+                                    {!this.props.doc.autoId && <input type="text" value={this.props.doc.id} onChange={this.onIdChange} placeholder="ID" />}
+                                </div>
+                                <div className="small-5 columns">
+                                    <label>Exam board:</label>
+                                    <select value={this.props.doc.examBoard} onChange={this.onExamBoardChange}>
+                                        <option value="">Any</option>
+                                        <option value="AQA">AQA</option>
+                                        <option value="OCR">OCR</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="row">
                             </div>
                         </div>
-                        <div className="small-9 columns" >
+                        <div className="small-7 columns" >
                             <textarea value={this.props.doc.explanation.value || ''} rows="6" onChange={this.onExplanationChange}></textarea>
                         </div>
                     </Block>
