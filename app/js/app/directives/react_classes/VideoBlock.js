c7fc529cb8cd7044e93fd625d0fafa772911ae10
Ben Hanson <bh412@cam.ac.uk>
2021-01-25 14:21:54 +0000

    Make alt text more obvious to change

--- app/js/app/directives/react_classes/VideoBlock.js
+++ app/js/app/directives/react_classes/VideoBlock.js
@@ -6,27 +6,32 @@ define(["react", "jquery"], function(React,$) {
 				this.props.onChange(this, oldDoc, newDoc);
 			},
 
-			onAltTextChange: function(c, oldVal, newVal, oldChildren, newChildren) {
+			onAltTextChange: function(e) {
+
+				this.setState({
+					altText: e.target.value
+				});
+
 				var oldDoc = this.props.doc;
 				var newDoc = $.extend({}, this.props.doc);
-				newDoc.altText = newVal;
-				newDoc.children = newChildren;
+				newDoc.altText = e.target.value;
 
 				this.onDocChange(this, oldDoc, newDoc);
 			},
 
 			render: function() {
 
-				var optionalAltText = <ContentValueOrChildren value={this.props.doc.altText} children={this.props.doc.children} encoding={this.props.doc.encoding} onChange={this.onAltTextChange}/>;
-
 				return (
 					<Block type="video" blockTypeTitle="Video" doc={this.props.doc} onChange={this.onDocChange}>
 						<div className="row">
-							<div className="small-6 columns text-center">
+							<div className="small-4 columns text-center">
 								Video src: {this.props.doc.src}
 							</div>
-							<div className="small-6 columns">
-								{optionalAltText}
+							<div className="small-3 columns text-right">
+								Alt Text:
+							</div>
+							<div className="small-5 columns">
+								<input type="text" placeholder="Enter alt text" value={this.props.doc.altText} onChange={this.onAltTextChange} />
 							</div>
 						</div>
 					</Block>
