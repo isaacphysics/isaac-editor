5591b3b77975cc0b50bac82556db80e3240b5d3f
James Sharkey <jsharkey13@users.noreply.github.com>
2021-07-06 10:03:13 +0100

    Use correct Anvil app embed URL
    The old style of running apps from anvil.works is not allowed for security
    reasons and hasn't worked in a long time!

--- app/js/app/directives/react_classes/AnvilAppBlock.js
+++ app/js/app/directives/react_classes/AnvilAppBlock.js
@@ -19,7 +19,7 @@ define(["react", "jquery"], function(React,$) {
 				return (
 					<Block type="anvilApp" blockTypeTitle="Anvil App" doc={this.props.doc} onChange={this.onDocChange}>
 						<div style={{color: "#bbb"}} className="text-center"><i>App height will be correct on live pages.</i></div><br/><br/>
-						<iframe style={{width: "100%", height: "300px"}} src={"https://anvil.works/apps/" + this.props.doc.appId + "/" + this.props.doc.appAccessKey + "/app"}/>
+						<iframe style={{width: "100%", height: "300px"}} src={"https://" + this.props.doc.appId + ".anvil.app/" + this.props.doc.appAccessKey}/>
 					</Block>
 				);
 			}
